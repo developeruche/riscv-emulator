10cead3727a7907773415c7f80403211c63ac2f7
Nwele Uchenna (developeruche) <nweleuchenna2004@gmail.com>
2025-02-07 11:04:12 +0100

    mod: done with the elf parser

---
+++ crates/core/src/lib.rs
@@ -0,0 +1,7 @@
+/// This is the size of a word in bytes for this vm
+pub const WORD_SIZE: usize = 4;
+/// This is the maximum memory size for this vm
+pub const MAXIMUM_MEMORY_SIZE: u32 = u32::MAX;
+
+
+
