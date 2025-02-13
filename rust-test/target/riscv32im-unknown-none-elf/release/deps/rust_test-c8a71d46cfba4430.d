db6d618ea7f412f32ae8b6c09bf8e50cad3b9f7c
Nwele Uchenna (developeruche) <nweleuchenna2004@gmail.com>
2025-02-13 03:25:20 +0100

    mod: done with the clean up

--- rust-test/target/riscv32im-unknown-none-elf/release/deps/rust_test-c8a71d46cfba4430.d
+++
@@ -1,5 +0,0 @@
-/Users/user/Documents/projects/RUST/risc-v-emulator/rust-test/target/riscv32im-unknown-none-elf/release/deps/rust_test-c8a71d46cfba4430: src/main.rs
-
-/Users/user/Documents/projects/RUST/risc-v-emulator/rust-test/target/riscv32im-unknown-none-elf/release/deps/rust_test-c8a71d46cfba4430.d: src/main.rs
-
-src/main.rs:
--- rust-test/target/riscv32im-unknown-none-elf/release/rust-test.d
+++
@@ -1,1 +0,0 @@
-/Users/user/Documents/projects/RUST/risc-v-emulator/rust-test/target/riscv32im-unknown-none-elf/release/rust-test: /Users/user/Documents/projects/RUST/risc-v-emulator/rust-test/src/main.rs
